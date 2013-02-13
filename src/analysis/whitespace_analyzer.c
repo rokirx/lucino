647833cb2d0b1da7c3f80e52e76f74810e659515
Robert Kirchgessner <rokigoro@googlemail.com>
2013-02-14 00:12:54 +0100

    minor cleanup

--- src/analysis/whitespace_analyzer.c
+++ src/analysis/whitespace_analyzer.c
@@ -7,38 +7,25 @@ BEGIN_C_DECLS
 
 static apr_status_t
 lcn_whitespace_analyzer_token_stream( lcn_analyzer_t* analyzer,
-				      lcn_token_stream_t** token_stream,
-				      const char* input,
+                                      lcn_token_stream_t** token_stream,
+                                      const char* input,
                                       apr_pool_t *pool )
 {
-    apr_status_t s;
-    
-    do
-    {
-	LCNCE( lcn_whitespace_tokenizer_create( token_stream,
-						input,
-						pool ) );
-						
-    }
-    while( FALSE );
-
-    return s;
+    return lcn_whitespace_tokenizer_create( token_stream, input, pool );
 }
 
 apr_status_t
 lcn_whitespace_analyzer_create( lcn_analyzer_t** analyzer,
-				apr_pool_t* pool )
+                                apr_pool_t* pool )
 {
     apr_status_t s;
-	
+
     do
     {
-	LCNPV( (*analyzer) = apr_pcalloc( pool,
-					  sizeof( lcn_analyzer_t ) ),
-	    APR_ENOMEM );
+        LCNPV( *analyzer = lcn_object_create(lcn_analyzer_t, pool ), APR_ENOMEM );
 
-	(*analyzer)->pool = pool;
-	(*analyzer)->token_stream = lcn_whitespace_analyzer_token_stream;
+        (*analyzer)->pool = pool;
+        (*analyzer)->token_stream = lcn_whitespace_analyzer_token_stream;
         (*analyzer)->type = "lcn_whitespace_analyzer";
     }
     while( FALSE );
--- src/analysis/whitespace_tokenizer.c
+++ src/analysis/whitespace_tokenizer.c
@@ -1,7 +1,6 @@
 #include "char_tokenizer.h"
 
 
-
 BEGIN_C_DECLS
 
 static lcn_bool_t
@@ -12,19 +11,15 @@ lcn_whitespace_tokenizer_is_token_char( char c )
 
 apr_status_t
 lcn_whitespace_tokenizer_create( lcn_token_stream_t** token_stream,
-				 const char* string,
-				 apr_pool_t* pool )
+                                 const char* string,
+                                 apr_pool_t* pool )
 {
     apr_status_t s;
 
     do
     {
-	LCNCE( lcn_char_tokenizer_create( token_stream,
-					  string,
-					  pool ) );
-
-	(*token_stream)->is_token_char = lcn_whitespace_tokenizer_is_token_char;
-	
+        LCNCE( lcn_char_tokenizer_create( token_stream, string, pool ) );
+        (*token_stream)->is_token_char = lcn_whitespace_tokenizer_is_token_char;
     }
     while( FALSE );
 
